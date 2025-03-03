0c59a01a0d70e93eb40fd125f9b0b912a3c82a50
younha00 <ayeon0@office.skhu.ac.kr>
2025-03-03 23:00:25 +0900

    query로 요청 보내보기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 import Roles from './components/roles'
 import Teams from './components/teams'
@@ -8,6 +10,11 @@ import People from './components/people'
 function App() {
 
   const [menu, setMenu] = useState('Roles')
+
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache()
+  });
 
   let mainComp = {
     Roles: (<Roles />),
@@ -27,19 +34,21 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Company Management</h1>
-        <nav>
-          <ul>
-            {NavMenus()}
-          </ul>
-        </nav>
-      </header>
-      <main>
-        {mainComp[menu]}
-      </main>
-    </div>
+      <div className="App">
+        <ApolloProvider client={client}>
+          <header className="App-header">
+            <h1>Company Management</h1>
+            <nav>
+              <ul>
+                {NavMenus()}
+              </ul>
+            </nav>
+          </header>
+          <main>
+            {mainComp[menu]}
+          </main>
+        </ApolloProvider>
+      </div>
   );
 }
 
